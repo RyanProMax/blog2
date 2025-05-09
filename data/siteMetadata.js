83d92adcea2df4486e3b5e83da8264d8c2fa970d
Ryan <627311610@qq.com>
2025-05-10 00:47:53 +0800

    fix: profile & remove blogs

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: `Ryan's Blog`,
+  author: 'Ryan',
+  headerTitle: `Ryan's Blog`,
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
